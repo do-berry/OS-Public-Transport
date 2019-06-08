f97f2ec30e3c76335e0a6a75f0a531a309e1007c
domi <dominikadolik6@gmail.com>
2019-06-08 22:19:26 +0200

    I'm going through changeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeees

--- main.cpp
+++ main.cpp
@@ -9,70 +9,203 @@
 #include <atomic>
 #include "Passenger.h"
 #include "Bus.h"
-#include "Screen.h"
-#include "Lights.h"
- 
+
 using namespace std;
 
-atomic<bool> run (false);
+atomic<bool> run(false);
 atomic<int> A{0}, B{0};
-Screen * screen = new Screen();
-//Passenger * passengers [howMany];         // pasazerowie
-Bus * bus = new Bus();                                // autobus
-thread * threads [2];               // pasazerowie + autobus
-Lights * lights = new Lights();                          // swiatla
+Bus *bus = new Bus();
+thread *busThread;
+thread *lights;
+WINDOW *windows[3];
+mutex writeMutex;
+mutex busMutex, aMutex, bMutex, roadMutex;
+condition_variable busCV, aCV, bCV;
+atomic<bool> firstLight;
+atomic<bool> secondLight;
 
-/*
-void * travel(int id) {
-  // todo
+// screen
+void write(int no, int line, string txt)
+{
+  writeMutex.lock();
+  mvwaddstr(windows[no], line, 0, txt.c_str());
+  wrefresh(windows[no]);
+  writeMutex.unlock();
 }
 
-void createPassengers() {
-  for (int i = 0; i < howMany; i++) {
-    passengers[i] = new Passenger();
-    if (passengers[i]->busStop) {
-      ++A;
-      screen->writeToA(A);
-    } else {
-      ++B;
-      screen->writeToB(B);
+void writeToA(int x)
+{
+  string txt = "Na przystanku: " + to_string(x) + "\n";
+  write(0, 2, txt);
+}
+
+void writeToBus(int line, string txt)
+{
+  write(1, line, txt);
+}
+
+void writeToB(int x)
+{
+  string txt = "Na przystanku: " + to_string(x) + "\n";
+  write(2, 2, txt);
+}
+
+// bus
+void onTheRoad()
+{
+  writeToBus(2, "W trasie\n");
+  this_thread::sleep_for(chrono::milliseconds(2000));
+  bus->onRoad.store(true);
+  unique_lock<mutex> lck(busMutex);
+  if (firstLight == false)
+  {
+    string txt = "Stoi na czerwonym swietle\n";
+    writeToBus(2, txt);
+    while (firstLight.load() != true)
+    {
+      busCV.wait(lck);
     }
-    threads[i] = new thread(travel, i);
+  }
+  if (secondLight == false)
+  {
+    string txt = "Stoi na czerwonym swietle\n";
+    writeToBus(2, txt);
+    while (secondLight.load() != true)
+    {
+      busCV.wait(lck);
+    }
+  }
+  lck.unlock();
+}
+
+void *go()
+{
+  while (run)
+  {
+    // a
+    busMutex.lock();
+    bus->onRoad.store(false);
+    bus->atA.store(true);
+    bus->atB.store(false);
+    writeToBus(2, "Na przystanku A\n");
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    aCV.notify_all();
+    busMutex.unlock();
+    unique_lock<mutex> aLock(aMutex);
+    while (A.load() > 0)
+    {
+      aCV.wait(aLock);
+    }
+    aLock.unlock();
+    // trasa
+    onTheRoad();
+    // b
+    busMutex.lock();
+    bus->onRoad.store(false);
+    bus->atA.store(false);
+    bus->atB.store(true);
+    writeToBus(2, "Na przystanku B\n");
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    bCV.notify_all();
+    busMutex.unlock();
+    unique_lock<mutex> bLock(bMutex);
+    while (B.load() > 0)
+    {
+      bCV.wait(bLock);
+    }
+    bLock.unlock();
+    // trasa
+    onTheRoad();
   }
 }
-*/
-void createBus() {
-  threads[0] = new thread(&bus->go, screen, lights, run);
-  threads[1] = new thread(&lights->beOn, screen, bus, run);
+
+// lights
+void *beOn()
+{
+  while (run)
+  {
+    if (firstLight.load() == true)
+    {
+      firstLight.store(false);
+      secondLight.store(true);
+      writeToBus(8, "czerwone\n");
+      writeToBus(10, "zielone\n");
+      this_thread::sleep_for(chrono::milliseconds(3000));
+      roadMutex.lock();
+      if (bus->onRoad.load() == true)
+      {
+        busCV.notify_one();
+      }
+      roadMutex.unlock();
+    }
+    else
+    {
+      firstLight.store(true);
+      secondLight.store(false);
+      writeToBus(8, "zielone\n");
+      writeToBus(10, "czerwone\n");
+      this_thread::sleep_for(chrono::milliseconds(3000));
+      roadMutex.lock();
+      if (bus->onRoad.load() == true)
+      {
+        busCV.notify_one();
+      }
+      roadMutex.unlock();
+    }
+  }
 }
 
-void stop() {
-  /*for (int i = 0; i < howMany + 1; i++) {
-    threads[i]->join();
-    delete threads[i];
-    if (i < howMany) {
-      delete passengers[i];
-    }
-  }*/
-  run.store(false);
-  threads[0]->join();
-  threads[1]->join();
-  delete bus;
+// threads
+void createThreads()
+{
+  busThread = new thread(&go);
+  lights = new thread(&beOn);
+}
+
+void stopThreads()
+{
+  busThread->join();
+  delete busThread;
+  lights->join();
   delete lights;
-  delete screen;
+  delete bus;
 }
 
-int main() {
-  srand(time(NULL));
+// screen
+void init()
+{
+  int x, y;
+  initscr();
+  refresh();
+  getmaxyx(stdscr, y, x);
+  x /= 3;
+  windows[0] = newwin(y, x, 0, 0);
+  windows[1] = newwin(y, x, 0, x);
+  windows[2] = newwin(y, x, 0, 2 * x);
+  mvwaddstr(windows[0], 0, 0, "Przystanek A\n");
+  wrefresh(windows[0]);
+  mvwaddstr(windows[2], 0, 0, "Przystanek B\n");
+  wrefresh(windows[2]);
+  mvwaddstr(windows[1], 0, 0, "Autobus\n");
+  wrefresh(windows[1]);
   char choice = 0;
-  run.store(true);
-  createBus();
-  //createPassengers();
-  while (run) {
+  createThreads();
+  while (run)
+  {
     cin >> choice;
-    if (choice == 'n') {
-      stop();
+    if (choice == 'n')
+    {
+      run.store(false);
+      stopThreads();
     }
   }
+  endwin();
+}
+
+int main()
+{
+  srand(time(NULL));
+  run.store(true);
+  init();
   return 0;
 }
--- makefile
+++ makefile
@@ -3,10 +3,10 @@ DEFINES =
 CFLAGS = -std=c++11 -O0 -g
 LIB = -lpthread -lncurses
 
-main: Lights.o Bus.o Passenger.o Screen.o main.o
-	$(CC) -o main main.o Passenger.o Bus.o Screen.o Lights.o $(LIB)
+main: Bus.o Passenger.o main.o
+	$(CC) -o main main.o Passenger.o Bus.o $(LIB)
 
-main.o: main.cpp Passenger.h Bus.h Screen.h Lights.h
+main.o: main.cpp Passenger.h Bus.h
 	$(CC) -c $(DEFINES) $(CFLAGS) main.cpp -o main.o
 
 Passenger.o: Passenger.cpp Passenger.h
@@ -14,9 +14,3 @@ Passenger.o: Passenger.cpp Passenger.h
 
 Bus.o: Bus.cpp Bus.h
 	$(CC) -c $(DEFINES) $(CFLAGS) Bus.cpp -o Bus.o
-
-Screen.o: Screen.cpp Screen.h
-	$(CC) -c $(DEFINES) $(CFLAGS) Screen.cpp -o Screen.o
-
-Lights.o: Lights.cpp Lights.h
-	$(CC) -c $(DEFINES) $(CFLAGS) Lights.cpp -o Lights.o
